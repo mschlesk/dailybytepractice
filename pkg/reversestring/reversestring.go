e5b06dbced4d2b2c08e0f50a9a1f0238ea5aa76e
mschlesk <MarkSchleske@Gmail.com>
2020-09-20 23:28:16 -0600

    (feat) Implement the reverse string tests

--- pkg/reversestring/reversestring.go
+++ pkg/reversestring/reversestring.go
@@ -1,13 +1,15 @@
 package reversestring
 
+import "fmt"
+
 /*
 This question is asked by Google. Given a string, reverse all of its characters and return the resulting string.
 
 Ex: Given the following strings...
 
-“Cat”, return “taC”
-“The Daily Byte”, return "etyB yliaD ehT”
-“civic”, return “civic”
+"Cat", return "taC"
+"The Daily Byte", return "etyB yliaD ehT"
+"civic", return "civic"
 */
 
 func reverse(s string) string {
@@ -16,5 +18,19 @@ func reverse(s string) string {
 
 // Test runs given and extracurricular test cases against proposed solution
 func Test() bool {
-	return false
+	pass := true
+	tests := map[string]string{"Cat": "taC", "The Daily Byte": "etyB yliaD ehT", "civic": "civic"}
+
+	for test, expected := range tests {
+		fmt.Printf("\treverse(%s) == %s?", test, expected)
+
+		if reverse(test) == expected {
+			fmt.Println(" ✔️")
+		} else {
+			fmt.Println(" ❌")
+			pass = false
+		}
+
+	}
+	return pass
 }
