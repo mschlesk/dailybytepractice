d51addc57ef073775d926f672aea45a20bf02a40
mschlesk <MarkSchleske@Gmail.com>
2020-09-20 21:47:41 -0600

    (feat) Constrcut reverse bytes problem skeleton

---
+++ pkg/reversebyte/reversebyte.go
@@ -0,0 +1,20 @@
+package reversebyte
+
+/*
+This question is asked by Google. Given a string, reverse all of its characters and return the resulting string.
+
+Ex: Given the following strings...
+
+“Cat”, return “taC”
+“The Daily Byte”, return "etyB yliaD ehT”
+“civic”, return “civic”
+*/
+
+func reverse(s string) string {
+	return ""
+}
+
+// Test runs given and extracurricular test cases against proposed solution
+func Test() bool {
+	return false
+}
